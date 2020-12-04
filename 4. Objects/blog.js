6c6ad88e161f0ad3e738cbfb582e319ad4461fbb
nileshhazra <nileshhz99@gmail.com>
2020-12-04 19:11:10 +0530

    debug blog

--- 4. Objects/blog.js
+++ 4. Objects/blog.js
@@ -27,8 +27,8 @@ console.log(post1);
 
 //
 let priceRanges = [
-    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 }
-    { label: '$$', tooltip: 'Moderate', minPerPerson: 10, maxPerPerson: 20 }
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+    { label: '$$', tooltip: 'Moderate', minPerPerson: 10, maxPerPerson: 20 },
     { label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
 ];
 
