793564bc1908cb864015f5dc155539bf03e3c3ae
nileshhazra <nileshhz99@gmail.com>
2021-01-20 14:01:37 +0530

    minor edit

--- 4. Objects/string.js
+++ 4. Objects/string.js
@@ -1,5 +1,18 @@
-const message = 'hi bill';
+const message = "   This is my 'first' message   ";
 
 const another = new String('hi');
 
 console.log(message.includes('fi'));
+
+console.log(message.length);
+console.log(message[2]);
+console.log(message.includes('my'));
+console.log(message.startsWith('This'));
+console.log(message.endsWith('e'));
+console.log(message.indexOf('my'));
+let uppercase = message.toUpperCase();
+console.log(uppercase);
+let trimmed = message.trim();
+console.log(trimmed);
+let arr = trimmed.split(' ');
+console.log(arr);
